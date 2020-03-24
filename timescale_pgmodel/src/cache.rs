8d2c37b3231aa807b40940220114525a92e216c0
Joshua Lockerman <>
2020-03-24 16:43:17 -0400

    De-stub the cache

--- timescale_pgmodel/src/cache.rs
+++ timescale_pgmodel/src/cache.rs
@@ -1,18 +1,35 @@
 
+use chashmap::CHashMap;
+use once_cell::sync::Lazy;
+
 use crate::labels::Labels;
 
+static LABELS_CACHE: Lazy<CHashMap<Labels, i64>> = Lazy::new(CHashMap::new);
+
 pub fn get_id(labels: &Labels) -> Option<i64> {
-    todo!()
+    let guard = LABELS_CACHE.get(labels);
+    match guard {
+        Some(g) => Some(*g),
+        None => None,
+    }
 }
 
 pub fn set_id(labels: Labels, id: i64) {
-    todo!()
+    LABELS_CACHE.insert(labels, id);
 }
 
+static METRIC_TABLE_CACHE: Lazy<CHashMap<String, &'static str>> = Lazy::new(CHashMap::new);
+
 pub fn get_metric_table_name(name: &str) -> Option<&'static str> {
-    todo!()
+    let guard = METRIC_TABLE_CACHE.get(name);
+    match guard {
+        Some(g) => Some(*g),
+        None => None,
+    }
 }
 
 pub fn set_metric_table_name(name: String, table_name: String) -> &'static str {
-    todo!()
+    let table_name = Box::leak(table_name.into_boxed_str());
+    METRIC_TABLE_CACHE.insert(name, table_name);
+    table_name
 }
