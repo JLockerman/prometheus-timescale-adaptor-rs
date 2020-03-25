dc05b69ca8c1ad7d6e9cf5c9ea2b822f665ffebf
Joshua Lockerman <>
2020-03-25 14:21:21 -0400

    Add client for the adaptor library

---
+++ prometheus-timescale-adaptor-rs/src/main.rs
@@ -0,0 +1,123 @@
+
+use std::{
+    io::Read,
+    sync::Arc,
+};
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+
+use timescale_pgmodel::{
+    promb::remote::WriteRequest,
+    ingestor::Client
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    use clap::{App, Arg};
+
+    let matches = App::new("prometheus-timescale-adaptor")
+        .arg(Arg::with_name("host")
+            .short("h")
+            .long("host")
+            .value_name("hostname")
+            .help("hostname of the timescale instance to run against")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("port")
+            .help("port of the timescale instance to run against")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("user")
+            .short("u")
+            .long("user")
+            .value_name("user")
+            .help("username to log into the database as")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("database")
+            .short("d")
+            .long("database")
+            .value_name("database")
+            .help("database to run against")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("password")
+            .short("a")
+            .long("password")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+
+    let host = matches.value_of("host").unwrap();
+    let port = matches.value_of("port").unwrap();
+    let user = matches.value_of("user").unwrap();
+    let database = matches.value_of("database").unwrap();
+    let password = matches.value_of("password").unwrap();
+
+    let connection_str = format!("host={} port={} user={} dbname={} password='{}', connect_timeout=10",
+        host, port, user, database, password);
+    let (client, connection) =
+        tokio_postgres::connect(&*connection_str, tokio_postgres::NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let client = Arc::new(Client::from_pg_client(client).await?);
+
+    let service = make_service_fn(move |_| {
+        let client = client.clone();
+        async move {
+            Ok::<_, hyper::Error>(service_fn(move |req| write(client.clone(), req)))
+        }
+    });
+
+    let addr = ([127, 0, 0, 1], 9201).into();
+    let server = Server::bind(&addr).serve(service);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
+}
+
+async fn write(client: Arc<Client>, req: Request<Body>) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::POST, "/write") => {
+            let body = hyper::body::to_bytes(req.into_body()).await?;
+            let mut decompresser = snap::read::FrameDecoder::new(&*body);
+            let mut buffer = vec![];
+            decompresser.read_to_end(&mut buffer)?;
+            let decompressed = buffer.into();
+            let write_req: WriteRequest = protobuf::parse_from_carllerche_bytes(&decompressed)?;
+            match client.ingest(write_req.get_timeseries()).await {
+                Ok(_rows) => {
+                    let mut ok = Response::default();
+                    *ok.status_mut() = StatusCode::OK;
+                    Ok(ok)
+                }
+                Err(errors) => {
+                    eprintln!("pg errors: {:?}", errors);
+                    let mut err = Response::default();
+                    *err.status_mut() = StatusCode::BAD_REQUEST;
+                    Ok(err)
+                }
+            }
+
+        }
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
--- timescale_pgmodel/src/ingestor.rs
+++ timescale_pgmodel/src/ingestor.rs
@@ -8,6 +8,7 @@ use indexmap::IndexMap;
 use tokio_postgres::{
     Client as PgClient,
     Error as PgError,
+    Statement as PgStatement,
     binary_copy::BinaryCopyInWriter,
     types::Type as PgType,
 };
@@ -18,17 +19,87 @@ use crate::{
     promb::types::{Sample, TimeSeries},
 };
 
-pub type NewSeries = IndexMap<Labels, Vec<usize>>;
+type NewSeries = IndexMap<Labels, Vec<usize>>;
 
 struct Samples<'s>(i64, &'s [Sample]);
 
-pub async fn ingest(client: &PgClient, time_series: &[TimeSeries]) -> Result<u64, Vec<PgError>> {
-    let (new_series, mut samples) = parse_data(time_series).unwrap();
+pub struct Client {
+    pg_client: PgClient,
+    get_id_for_labels: PgStatement,
+    get_metrics_table: PgStatement,
+}
 
-    insert_series(client, new_series, &mut samples).await?;
+impl Client {
+    pub async fn from_pg_client(pg_client: PgClient) -> Result<Self, PgError> {
+        let get_id_for_labels = pg_client.prepare("SELECT get_series_id_for_key_value_array($1, $2, $3)")
+            .await?;
+        let get_metrics_table = pg_client.prepare("SELECT get_series_id_for_key_value_array($1, $2, $3)")
+            .await?;
+        Ok(Self{ pg_client, get_id_for_labels, get_metrics_table, })
+    }
 
-    let rows = insert_data(client, samples).await?;
-    Ok(rows)
+    pub async fn ingest(&self, time_series: &[TimeSeries]) -> Result<u64, Vec<PgError>> {
+        let (new_series, mut samples) = parse_data(time_series).unwrap();
+
+        self.insert_series(new_series, &mut samples).await?;
+
+        let rows = self.insert_data(samples).await?;
+        Ok(rows)
+    }
+
+    async fn insert_series(&self, mut series: NewSeries, samples: &mut HashMap<String, Vec<Samples<'_>>>)
+    -> Result<(), Vec<PgError>> {
+        series.sort_keys();
+        let mut inserts = FuturesUnordered::from_iter(series.into_iter()
+            .map(|(l, i)| (self, l, i))
+            .map(series_insert_query)
+        );
+        let mut errs = vec![];
+        while let Some(res) = inserts.next().await {
+            let (id, label, idxs) = match res {
+                Ok(r) => r,
+                Err(e) => {
+                    errs.push(e);
+                    continue // we still want o populate the cache with any new ids
+                }
+            };
+            let samples = samples.get_mut(label.metric_name()).unwrap();
+            for idx in idxs {
+                samples[idx].0 = id
+            }
+            cache::set_id(label, id);
+        }
+        if !errs.is_empty() {
+            return Err(errs)
+        }
+        Ok(())
+    }
+
+    async fn insert_data(&self, rows: HashMap<String, Vec<Samples<'_>>>)
+    -> Result<u64, Vec<PgError>> {
+        FuturesUnordered::from_iter(rows.into_iter()
+                .map(|(m, s)| (self, m, s))
+                .map(insert_data_query)
+            ).fold(Ok(0), |rows, res| async {
+                match (rows, res) {
+                    (Ok(rows), Ok(count)) => Ok(rows + count),
+                    (Err(es), Ok(..)) => Err(es),
+                    (Ok(..), Err(e)) => Err(vec![e]),
+                    (Err(mut es), Err(e)) => {
+                        es.push(e);
+                        Err(es)
+                    },
+                }
+            })
+            .await
+    }
+
+    async fn get_metric_table_name(&self, metric: String) -> Result<&'static str, PgError> {
+        let row = self.pg_client.query_one(&self.get_metrics_table, &[&metric]).await?;
+        let name: String = row.get(0);
+        let table_name = cache::set_metric_table_name(metric, name);
+        Ok(table_name)
+    }
 }
 
 fn parse_data(time_series: &[TimeSeries])
@@ -57,69 +128,22 @@ fn parse_data(time_series: &[TimeSeries])
     Ok((new_series, samples))
 }
 
-async fn insert_series(client: &PgClient, mut series: NewSeries, samples: &mut HashMap<String, Vec<Samples<'_>>>)
--> Result<(), Vec<PgError>> {
-    series.sort_keys();
-    let mut inserts = FuturesUnordered::from_iter(series.into_iter()
-        .map(|(l, i)| (client, l, i))
-        .map(series_insert_query)
-    );
-    let mut errs = vec![];
-    while let Some(res) = inserts.next().await {
-        let (id, label, idxs) = match res {
-            Ok(r) => r,
-            Err(e) => {
-                errs.push(e);
-                continue // we still want o populate the cache with any new ids
-            }
-        };
-        let samples = samples.get_mut(label.metric_name()).unwrap();
-        for idx in idxs {
-            samples[idx].0 = id
-        }
-        cache::set_id(label, id);
-    }
-    if !errs.is_empty() {
-        return Err(errs)
-    }
-    Ok(())
-}
-
-async fn series_insert_query((client, label, idxs): (&PgClient, Labels, Vec<usize>))
+async fn series_insert_query((client, label, idxs): (&Client, Labels, Vec<usize>))
 -> Result<(i64, Labels, Vec<usize>), PgError> {
-    static GetSeriesIDForLabelSQL: &str = "SELECT get_series_id_for_key_value_array($1, $2, $3)";
     let (names, values, metric_name) = label.components();
-    let row = client.query_one(GetSeriesIDForLabelSQL, &[&metric_name, &names, &values]).await?;
+    let row = client.pg_client.query_one(&client.get_id_for_labels, &[&metric_name, &names, &values])
+        .await?;
     let id: i64 = row.get(0);
     Ok((id, label, idxs))
 }
 
-async fn insert_data(client: &PgClient, rows: HashMap<String, Vec<Samples<'_>>>)
--> Result<u64, Vec<PgError>> {
-    FuturesUnordered::from_iter(rows.into_iter()
-            .map(|(m, s)| (client, m, s))
-            .map(insert_data_query)
-        ).fold(Ok(0), |rows, res| async {
-            match (rows, res) {
-                (Ok(rows), Ok(count)) => Ok(rows + count),
-                (Err(es), Ok(..)) => Err(es),
-                (Ok(..), Err(e)) => Err(vec![e]),
-                (Err(mut es), Err(e)) => {
-                    es.push(e);
-                    Err(es)
-                },
-            }
-        })
-        .await
-}
-
-async fn insert_data_query((client, metric, samples): (&PgClient, String, Vec<Samples<'_>>)) -> Result<u64, PgError> {
+async fn insert_data_query((client, metric, samples): (&Client, String, Vec<Samples<'_>>)) -> Result<u64, PgError> {
     let table_name = cache::get_metric_table_name(&metric);
     let table_name  = match table_name {
         Some(name) => name,
-        None => get_metric_table_name(client, metric).await?,
+        None => client.get_metric_table_name(metric).await?,
     };
-    let sink = client.copy_in(&*format!("COPY prom.{} FROM stdin BINARY", table_name)).await?;
+    let sink = client.pg_client.copy_in(&*format!("COPY prom.{} FROM stdin BINARY", table_name)).await?;
     let writer = BinaryCopyInWriter::new(sink, &[PgType::TIMESTAMPTZ, PgType::FLOAT8, PgType::INT8]);
     pin_mut!(writer);
     for Samples(id, samples) in samples {
@@ -129,12 +153,4 @@ async fn insert_data_query((client, metric, samples): (&PgClient, String, Vec<Sa
         }
     }
     writer.finish().await
-}
-
-async fn get_metric_table_name(client: &PgClient, metric: String) -> Result<&'static str, PgError> {
-    static GetCreateMetricsTableSQL: &str = "SELECT get_series_id_for_key_value_array($1, $2, $3)";
-    let row = client.query_one(GetCreateMetricsTableSQL, &[&metric]).await?;
-    let name: String = row.get(0);
-    let table_name = cache::set_metric_table_name(metric, name);
-    Ok(table_name)
 }
